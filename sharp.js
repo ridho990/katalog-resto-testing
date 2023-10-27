0c364fb2b374a999920d4664ea4d7fc802a58b8c
Ridho Ahmad Fauzi <ridho.ahmad21@students.unila.ac.id>
2023-10-27 23:44:58 +0700

    Menambakan image responsive

--- sharp.js
+++ sharp.js
@@ -2,37 +2,26 @@ const sharp = require('sharp');
 const fs = require('fs');
 const path = require('path');
 
-const target = path.resolve(__dirname, 'src/public/images');
-const destination = path.resolve(__dirname, 'dist/images');
+const target = path.resolve(__dirname, 'src/public/images/heros');
+const destination = path.resolve(__dirname, 'src/public/images/heros');
 
 if (!fs.existsSync(destination)) {
     fs.mkdirSync(destination);
 }
 
 fs.readdirSync(target).forEach((image) => {
-    // mengubah ukuran gambar dengan lebar 800px, dengan prefix -large.jpg
+    // mengubah ukuran gambar dengan lebar 1200px, dengan prefix -large.jpg
     sharp(`${target}/${image}`)
-        .resize(800)
-        .toFile(
-            path.resolve(
-                __dirname,
-                `${destination}/${image
-                    .split('.')
-                    .slice(0, -1)
-                    .join('.')}-large.jpg`,
-            ),
-        );
+        .resize(1200)
+        .toFile(path.resolve(__dirname, `${destination}/${image.split('.').slice(0, -1).join('.')}-large.jpg`));
 
-    // mengubah ukuran gambar dengan lebar 480px, dengan prefix -small.jpg
+    // mengubah ukuran gambar dengan lebar 900px, dengan prefix -medium.jpg
     sharp(`${target}/${image}`)
-        .resize(480)
-        .toFile(
-            path.resolve(
-                __dirname,
-                `${destination}/${image
-                    .split('.')
-                    .slice(0, -1)
-                    .join('.')}-small.jpg`,
-            ),
-        );
+        .resize(900)
+        .toFile(path.resolve(__dirname, `${destination}/${image.split('.').slice(0, -1).join('.')}-medium.jpg`));
+
+    // mengubah ukuran gambar dengan lebar 600px, dengan prefix -small.jpg
+    sharp(`${target}/${image}`)
+        .resize(600)
+        .toFile(path.resolve(__dirname, `${destination}/${image.split('.').slice(0, -1).join('.')}-small.jpg`));
 });
